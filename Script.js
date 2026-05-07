f1388d6f816f91ac5ccaba68fb7b7a40c438e2b2
Baileydog23aa <bhwill@stpeters.school.nz>
2026-05-08 10:55:47 +1200

    Finished Nav Menu

--- Script.js
+++ Script.js
@@ -1,7 +1,18 @@
+let viewing = false;
 document.querySelectorAll('#MenuBtn').forEach(btn => {
     btn.addEventListener('click', () => {
+        viewing = !viewing;
         document.querySelector('.NavMenu').classList.toggle('viewing');
         document.querySelector('.NavBG').classList.toggle('viewing');
         document.querySelector('.NavBG_Gradient').classList.toggle('viewing');
+        if (viewing == true) {
+            document.querySelector('#MenuBtn img').src = "Images/ExitButton.png";
+            document.querySelector('.exitBtn').classList.add('opening');
+            document.querySelector('.exitBtn').classList.remove('closing');
+        } else {
+            document.querySelector('#MenuBtn img').src = "Images/MenuButton.png";
+            document.querySelector('.exitBtn').classList.add('closing');
+            document.querySelector('.exitBtn').classList.remove('opening');
+        }
     });
 });
