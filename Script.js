72e16fa216787730ae42598107491f95f03e4372
Baileydog23aa <bhwill@stpeters.school.nz>
2026-05-13 15:06:25 +1200

    Settings and Most of Flight Page Complete

--- Script.js
+++ Script.js
@@ -16,3 +16,97 @@ document.querySelectorAll('#MenuBtn').forEach(btn => {
         }
     });
 });
+
+GPS_Status = document.getElementById('GPS_Status');
+if (GPS_Status.innerHTML == "HOLD") {
+    GPS_Status.style.color = "red";
+} else if (GPS_Status.innerHTML == "GO") {
+    GPS_Status.style.color = "#00FF00";
+}
+
+Flight_State = document.getElementById('Flight_State');
+if (Flight_State.innerHTML == "STARTUP") {
+    Flight_State.style.color = "red";
+} else if (Flight_State.innerHTML == "PAD_IDLE") {
+    Flight_State.style.color = "orange";
+} else if (Flight_State.innerHTML == "PAD_ARMED") {
+    Flight_State.style.color = "#00FF00";
+} else if (Flight_State.innerHTML == "ASCENT") {
+    Flight_State.style.color = "#ffee00";
+} else if (Flight_State.innerHTML == "BURNOUT") {
+    Flight_State.style.color = "#ff9900";
+} else if (Flight_State.innerHTML == "DESCENT") {
+    Flight_State.style.color = "#00ffff";
+} else if (Flight_State.innerHTML == "DROUGE_DEPLOY") {
+    Flight_State.style.color = "#0000ff";
+} else if (Flight_State.innerHTML == "DROUGE") {
+    Flight_State.style.color = "#ff00ff";
+} else if (Flight_State.innerHTML == "MAIN_DEPLOY") {
+    Flight_State.style.color = "#b300ff";
+} else if (Flight_State.innerHTML == "MAIN") {
+    Flight_State.style.color = "#6600ff";
+} else if (Flight_State.innerHTML == "BALLISTIC") {
+    Flight_State.style.color = "#ff0000";
+} else if (Flight_State.innerHTML == "LANDED") {
+    Flight_State.style.color = "#00ff00";
+}
+
+SD_State = document.getElementById('SD_State');
+if (SD_State.innerHTML == "DISABLED") {
+    SD_State.style.color = "orange";
+} else if (SD_State.innerHTML == "ERROR_RW") {
+    SD_State.style.color = "red";
+} else if (SD_State.innerHTML == "GO") {
+    SD_State.style.color = "#00FF00";
+}
+
+Flash_State = document.getElementById('Flash_State');
+if (Flash_State.innerHTML == "DISABLED") {
+    Flash_State.style.color = "orange";
+} else if (Flash_State.innerHTML == "ERROR_RW") {
+    Flash_State.style.color = "red";
+} else if (Flash_State.innerHTML == "GO") {
+    Flash_State.style.color = "#00FF00";
+}
+
+Data_Status = document.getElementById('Data_Status');
+if (Data_Status.innerHTML == "HOLD") {
+    Data_Status.style.color = "red";
+} else if (Data_Status.innerHTML == "GO") {
+    Data_Status.style.color = "#00FF00";
+}
+
+GyroActive = document.getElementById('GYRO');
+if (GyroActive.innerHTML == "GO") {
+    GyroActive.style.color = "#00FF00";
+} else if (GyroActive.innerHTML == "HOLD") {
+    GyroActive.style.color = "red";
+}
+
+AccelActive = document.getElementById('ACCEL');
+if (AccelActive.innerHTML == "GO") {
+    AccelActive.style.color = "#00FF00";
+} else if (AccelActive.innerHTML == "HOLD") {
+    AccelActive.style.color = "red";
+}
+
+HighRangeAccelActive = document.getElementById('HIGH_RANGE_ACCEL');
+if (HighRangeAccelActive.innerHTML == "GO") {
+    HighRangeAccelActive.style.color = "#00FF00";
+} else if (HighRangeAccelActive.innerHTML == "HOLD") {
+    HighRangeAccelActive.style.color = "red";
+}
+
+BAROActive = document.getElementById('BARO');
+if (BAROActive.innerHTML == "GO") {
+    BAROActive.style.color = "#00FF00";
+} else if (BAROActive.innerHTML == "HOLD") {
+    BAROActive.style.color = "red";
+}
+
+Flags_State = document.getElementById('Flags_Status');
+if (Flags_State.innerHTML == "GO") {
+    Flags_State.style.color = "#00FF00";
+} else if (Flags_State.innerHTML == "HOLD") {
+    Flags_State.style.color = "red";
+}
