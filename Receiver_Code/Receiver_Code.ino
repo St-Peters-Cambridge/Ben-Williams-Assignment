589c2364e4076b10c28a8d8dbdd636347c7da642
Baileydog23aa <bhwill@stpeters.school.nz>
2026-05-19 11:01:55 +1200

    Added Receiver Code

---
+++ Receiver_Code/Receiver_Code.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(0, OUTPUT);
+  digitalWrite(0, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
